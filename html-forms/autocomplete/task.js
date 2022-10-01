18c91927858d2f97866f907b5e0be26f4efce787
Виктория Дворникова <wika.daw@mail.ru>
2022-10-01 11:22:52 +0300

    Домашняяя работа 5.3

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,26 +68,22 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    let options = Array.from(this.input.options)
+    let arr = [];
+    let objectOptions;
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    for (let item of options) {
+      if(item.textContent.includes(text)) {
+        objectOptions = {
+          text:item.textContent,
+          value: item.value,
+        }
+        arr.push(objectOptions)
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    return arr;
   }
 }
-
 new Autocomplete( document.querySelector( '.autocomplete' ));
+
+
